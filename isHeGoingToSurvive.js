da15e3d1a858040db7ec08a02fde2171b97b8908
Owen Scott <oscott0202@gmail.com>
2025-03-17 09:48:09 -0400

    Code Wars Is He Gonna Survive Solution

---
+++ isHeGoingToSurvive.js
@@ -0,0 +1,11 @@
+function hero(bullets, dragons){
+    //Get Coding!
+      if(bullets /2 >= dragons){
+        return true
+    }else {
+     return false}
+    }
+    //or
+    function hero(bullets, dragons){
+        return bullets /2 >= dragons 
+        }
